e5f98d44c309f9f5da10375f589bb784a117d50c
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-12-01 23:37:24 -0500

    Get room setpoint and display in marker popup; Fix some indentation

--- public/app/services/FloorDataService.js
+++ public/app/services/FloorDataService.js
@@ -1,68 +1,75 @@
 app.service('FloorDataService', function (LoadingService, $http, $q) {
-    var self = this;
+  var self = this;
 
-    self.availableDates = []; // [2013-06-06 00:00:00", "2013-06-06 01:00:00", ...] (comes from weather data)
-    self.weatherData = {};    // {"2013-01-01 01:00:00": "37.04", ...}
+  self.availableDates = []; // [2013-06-06 00:00:00", "2013-06-06 01:00:00", ...] (comes from weather data)
+  self.weatherData = {};    // {"2013-01-01 01:00:00": "37.04", ...}
 
-    self.roomTempData = {};    // {vav: {date: temp, ...}, vav: {date: temp, ...}} (all room data for said floor)
-    self.roomCoordinates = {}; // {"10.65.06": [[601,  59], [636, 82]], ...}
-    self.vavs = {};            // {"47102": ["10.S.J"], ...}
+  self.roomTempData = {};    // {vav: {date: temp, ...}, vav: {date: temp, ...}} (all room data for said floor)
+  self.roomSetpointData = {}; // {vav: {date: setpoint, ... }, vav: {date: setpoint, .. }} (all setpoints for said floor)
+  self.roomCoordinates = {}; // {"10.65.06": [[601,  59], [636, 82]], ...}
+  self.vavs = {};            // {"47102": ["10.S.J"], ...}
 
-
-    self.getCoordinates = function getCoordinates(floorLevel) {
-        return $http.post('/api/v1/coordinates', {'floorLevel': floorLevel}).then(function(response) {
-            self.roomCoordinates = response.data['roomCoordinates'];
-            self.vavs = response.data['vavs'];
-        });
-    };
+  self.getCoordinates = function getCoordinates(floorLevel) {
+    return $http.post('/api/v1/coordinates', {'floorLevel': floorLevel}).then(function(response) {
+      self.roomCoordinates = response.data['roomCoordinates'];
+      self.vavs = response.data['vavs'];
+    });
+  };
 
   /**
-   * Initializes roomTempData
-   * roomTempData format:
-   * {
-   *    sensorDeviceId: { date: temperature, date: temperature, ... },
-   *    ...
-   * }
-   * @param {string} floorLevel - floor_{roomNumber}
-   * @returns {*|Promise.<TResult>}
-   */
-    self.getRoomTempData = function (floorLevel) {
-      const floorNumber = floorLevel.match(/\d+/g)[0];
-      return $http.get(`http://bpl.dev.cisdd.org/api/floor-data/${floorNumber}/vavs`).then(function(response) {
-        const roomTemps = response.data.vavs.filter(function (vav) {
-          return vav.sensorType === 'Room Temperature';
-        });
-
-        roomTemps.forEach(function (roomTemp) {
-          self.roomTempData[roomTemp.sensorDeviceId] = roomTemp.data;
-        });
-        LoadingService.makingRequest = false;
+  * Initializes room data
+  * data format:
+  * {
+  *    sensorDeviceId: { date: dataPoint, date: dataPoint, ... },
+  *    ...
+  * }
+  * @param {string} floorLevel - floor_{roomNumber}
+  * @returns {*|Promise.<TResult>}
+  */
+  self.getAllData = function (floorLevel) {
+    const floorNumber = floorLevel.match(/\d+/g)[0];
+    return $http.get(`http://bpl.dev.cisdd.org/api/floor-data/${floorNumber}/vavs`).then(function(response) {
+      const roomTemps = response.data.vavs.filter(function (vav) {
+        return vav.sensorType === 'Room Temperature';
       });
-    };
+      roomTemps.forEach(function (roomTemp) {
+        self.roomTempData[roomTemp.sensorDeviceId] = roomTemp.data;
+      });
 
-    self.getWeatherData = function getWeatherData() {
-        return $http.get('http://bpl.dev.cisdd.org/api/weather').then(function (response) {
-            self.weatherData = {};
-            let weatherResponse = response.data;
+      const roomSetpoints = response.data.vavs.filter(function (vav) {
+        return vav.sensorType === 'Room Setpoint';
+      });
+      roomSetpoints.forEach(function (roomSetpoint) {
+        self.roomSetpointData[roomSetpoint.sensorDeviceId] = roomSetpoint.data;
+      });
 
-            for (let i = 0; i < weatherResponse.length; ++i) {
-              let date = weatherResponse[i].stime,
-                  temp = weatherResponse[i].temp;
-              self.weatherData[date] = temp;
-            }
-            self.availableDates = Object.keys(self.weatherData);
-        });
-    };
+      LoadingService.makingRequest = false;
+    });
+  };
 
-    self.getAllFloorData = function (floorLevel) {
-        LoadingService.makingRequest = true;
-        
-        var promises = [
-          self.getCoordinates(floorLevel),
-          self.getRoomTempData(floorLevel),
-          self.getWeatherData()
-        ];
+  self.getWeatherData = function getWeatherData() {
+    return $http.get('http://bpl.dev.cisdd.org/api/weather').then(function (response) {
+      self.weatherData = {};
+      let weatherResponse = response.data;
 
-        return $q.all(promises);
-    }
+      for (let i = 0; i < weatherResponse.length; ++i) {
+        let date = weatherResponse[i].stime,
+            temp = weatherResponse[i].temp;
+        self.weatherData[date] = temp;
+      }
+      self.availableDates = Object.keys(self.weatherData);
+    });
+  };
+
+  self.getAllFloorData = function (floorLevel) {
+    LoadingService.makingRequest = true;
+
+    var promises = [
+      self.getCoordinates(floorLevel),
+      self.getAllData(floorLevel),
+      self.getWeatherData()
+    ];
+
+    return $q.all(promises);
+  }
 });
--- public/app/services/MapInteractionService.js
+++ public/app/services/MapInteractionService.js
@@ -1,90 +1,102 @@
 app.service('MapInteractionService', function (TableToMapService, FloorDataService, leafletData, $rootScope) {
-    var self = this;
+  var self = this;
 
-    // container for layers
-    self.vectorLayers = {};
+  // container for layers
+  self.vectorLayers = {};
 
-    // add specific VAV box to map
-    self.addMarkersToMap = function (date) {
-        $rootScope.displayDate = date;
+  // add specific VAV box to map
+  self.addMarkersToMap = function (date) {
+    $rootScope.displayDate = date;
 
-        leafletData.getMap('map').then(function (map) {
+    leafletData.getMap('map').then(function (map) {
 
-            // go through all vavs on floor
-            for (var vav in FloorDataService.vavs) {
+      // go through all vavs on floor
+      for (var vav in FloorDataService.vavs) {
 
-                // if the currentDate is in that vav set
-                if (date in FloorDataService.roomTempData[vav]) {
+        // if the currentDate is in that vav set
+        if (date in FloorDataService.roomTempData[vav]) {
 
-                    // get the markerValue of that vav
-                    var markerValue = self.getMarkerValue(vav, date);
-                    var color = TableToMapService.getColorFromRanges(markerValue).color;
+          // get the temp of that vav
+          var temp = self.getTemp(vav, date);
+          var color = TableToMapService.getColorFromRanges(temp).color;
 
-                    // go through rooms in vav box and add markers to map
-                    for (var i = 0; i < FloorDataService.vavs[vav].length; i++) {
+          // get the room setpoint of that vav
+          var roomSetpoint = self.getRoomSetpoint(vav, date);
 
-                        var coordinates = FloorDataService.roomCoordinates[FloorDataService.vavs[vav][i]];
-                        var layer = self.getMarkerType(coordinates, color, markerValue, map.getZoom());
+          // go through rooms in vav box and add markers to map
+          for (var i = 0; i < FloorDataService.vavs[vav].length; i++) {
 
-                        if (!self.vectorLayers.hasOwnProperty(vav)) {
-                            self.vectorLayers[vav] = [];
-                        }
+            var coordinates = FloorDataService.roomCoordinates[FloorDataService.vavs[vav][i]];
+            var layer = self.getMarkerType(coordinates, color, temp, roomSetpoint, map.getZoom());
 
-                        map.addLayer(layer);
-                        self.vectorLayers[vav].push(layer);
-                    }
-                }
-            }
-        });
-    };
-
-    // remove specific VAV Box from map
-    self.removeMarkersFromMap = function () {
-
-        leafletData.getMap('map').then(function (map) {
-            for (var vav in FloorDataService.vavs) {
-                for (var i = 0; i < self.vectorLayers[vav].length; i++) {
-                    map.removeLayer(self.vectorLayers[vav][i]);
-                }
-
-                delete self.vectorLayers[vav];
-            }
-        });
-    };
-
-    self.getMarkerType = function (coordinates, color, currentTemp, zoom) {
-        var degreeSign = String.fromCharCode(parseInt("00B0", 16));
-
-        var object = {
-            color: color,
-            fillOpacity: .5
-        };
-
-        if ($rootScope.marker_type === 'Circles') {
-            var latlng = L.latLng((coordinates[0][0] + coordinates[1][0]) / 2, (coordinates[0][1] + coordinates[1][1]) / 2);
-
-            // if zoom == 0
-            var radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * .6;
-
-            if (zoom == 1) {
-                radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * 1.2;
-            } else if (zoom == 2) {
-                radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * 2.4;
+            if (!self.vectorLayers.hasOwnProperty(vav)) {
+              self.vectorLayers[vav] = [];
             }
 
-            return new L.circleMarker(latlng, object).setRadius(radius).bindPopup(currentTemp + degreeSign);
-
-        } else if ($rootScope.marker_type === 'Squares') {
-            return new L.rectangle(coordinates, object).bindPopup(currentTemp + degreeSign);
+            map.addLayer(layer);
+            self.vectorLayers[vav].push(layer);
+          }
         }
+      }
+    });
+  };
+
+  // remove specific VAV Box from map
+  self.removeMarkersFromMap = function () {
+
+    leafletData.getMap('map').then(function (map) {
+      for (var vav in FloorDataService.vavs) {
+        for (var i = 0; i < self.vectorLayers[vav].length; i++) {
+          map.removeLayer(self.vectorLayers[vav][i]);
+        }
+
+        delete self.vectorLayers[vav];
+      }
+    });
+  };
+
+  self.getMarkerType = function (coordinates, color, currentTemp, currentSetpoint, zoom) {
+    var degreeSign = String.fromCharCode(parseInt("00B0", 16));
+    var popup = 'Current Temp: ' + currentTemp + degreeSign;
+    if (currentSetpoint) {
+      popup += '<br/>Setpoint: ' + currentSetpoint + degreeSign;
+    }
+
+    var object = {
+      color: color,
+      fillOpacity: .5
     };
 
-    self.getMarkerValue = function (vav, date) {
-        var roomTemp = FloorDataService.roomTempData[vav][date];
-        if ($rootScope.marker_options == 'Temp') {
-            return roomTemp;
-        } else if ($rootScope.marker_options == 'Temp: Inside Vs Outside') {
-            return Math.abs(FloorDataService.weatherData[date] - roomTemp) * 2;
-        }
-    };
+    if ($rootScope.marker_type === 'Circles') {
+      var latlng = L.latLng((coordinates[0][0] + coordinates[1][0]) / 2, (coordinates[0][1] + coordinates[1][1]) / 2);
+
+      // if zoom == 0
+      var radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * .6;
+
+      if (zoom == 1) {
+        radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * 1.2;
+      } else if (zoom == 2) {
+        radius = ((TableToMapService.getIndexOfColor(color) + 1) * 5) * 2.4;
+      }
+
+      return new L.circleMarker(latlng, object).setRadius(radius).bindPopup(popup);
+
+    } else if ($rootScope.marker_type === 'Squares') {
+      return new L.rectangle(coordinates, object).bindPopup(popup);
+    }
+  };
+
+  self.getTemp = function (vav, date) {
+    var roomTemp = FloorDataService.roomTempData[vav][date];
+    if ($rootScope.marker_options == 'Temp') {
+      return roomTemp;
+    } else if ($rootScope.marker_options == 'Temp: Inside Vs Outside') {
+      return Math.abs(FloorDataService.weatherData[date] - roomTemp) * 2;
+    }
+  };
+
+  self.getRoomSetpoint = function (vav, date) {
+    var roomSetpoint = FloorDataService.roomSetpointData[vav][date];
+    return roomSetpoint;
+  };
 });
